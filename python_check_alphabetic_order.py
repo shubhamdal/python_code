cb9c549cdaac4c9ae6cfdf403dd1911cd2c1bc84
shubham chawla <33931695+shubhamdal@users.noreply.github.com>
2020-02-18 00:38:16 +0530

    Update python_check_alphabetic_order.py

--- python_check_alphabetic_order.py
+++ python_check_alphabetic_order.py
@@ -1,5 +1,16 @@
 import sys
 
+def check_values(valu,alphabets_list):
+    for chrctr in valu:
+        if (chrctr in alphabets_list):
+            flag=True
+        else:
+            flag=False
+            return flag
+    return flag
+
+
+
 def main(alphabets_list,valu):  
     first_char=alphabets_list.index(valu[0])
     flag=0
@@ -10,9 +21,9 @@ def main(alphabets_list,valu):
         else:
             flag=1
     if (flag==0):
-    	print("\n\nThe word matches the (a-z) sequence\n\n")
+        print("\n\nThe word matches the (a-z) sequence\n\n")
     else:
-    	print("\n\nThe word DOES NOT matches the (a-z) sequence\n\n")
+        print("\n\nThe word DOES NOT matches the (a-z) sequence\n\n")
 
 
 if __name__ == "__main__":
@@ -22,11 +33,11 @@ if __name__ == "__main__":
     # chr() returns a string representing a character whose Unicode code point is an integer.
     print("The program checks if the sequence of characters in input word matches the actual alphabetic order (a-z)")
     while(1):
-        valu = input("Enter your Word : ") 
-        if (len(valu)>0):
-        	main(alphabets_list,valu)
-        	sys.exit(0)
+        valu = input("Enter your Word : ").lower()
+        if (len(valu)>0 and check_values(valu,alphabets_list)):
+            main(alphabets_list,valu)
+            sys.exit(0)
         else:
-        	print("Please enter your word")
+            print("Please enter your word (only alphabets allowed)")
 
     
